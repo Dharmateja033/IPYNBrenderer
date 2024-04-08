e50e3dd21b2a37fc12f0c508c471f361a11b533f
Dharma <dharmateja.e.97@gmail.com>
2024-04-08 11:23:34 +0530

    added comments on youtube testing

--- tests/unit/test_youtube_func.py
+++ tests/unit/test_youtube_func.py
@@ -19,10 +19,12 @@ URL_id_bad_data = [
 ]
 
 
-@pytest.mark.parametrize("URL, response", good_URL_data)
+@pytest.mark.parametrize("URL, response", good_URL_data)# This decorator tells pytest to run the test function multiple times, each time with different parameters specified in the good_URL_data list
 def test_get_time_info(URL, response):
     assert get_time_info(URL) == response
 
+""" This test function verifies that the get_time_info function correctly raises an InvalidURLException 
+    when provided with invalid YouTube URLs. It achieves this by using the pytest.raises context manager to check for the expected exception during the execution of the test."""
 @pytest.mark.parametrize("URL", URL_id_bad_data)
 def test_get_time_info_failed(URL):
     with pytest.raises(InvalidURLException):
