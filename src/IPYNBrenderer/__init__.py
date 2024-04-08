d9a67d0c117fe9ad59190e628a070acb655e1b0b
Dharma <dharmateja.e.97@gmail.com>
2024-04-08 17:42:51 +0530

    integration tested for youtube renderer

--- src/IPYNBrenderer/__init__.py
+++ src/IPYNBrenderer/__init__.py
@@ -1,3 +1,4 @@
-from src.IPYNBrenderer.youtube import render_Youtube_video, get_time_info
-from src.IPYNBrenderer.custom_exception import InvalidURLException
-#from IPYNBrenderer.site import render_site, is_valid
+from IPYNBrenderer.youtube import render_Youtube_video, get_time_info
+from IPYNBrenderer.custom_exception import InvalidURLException
+
+# from IPYNBrenderer.site import render_site, is_valid
--- tests/unit/test_youtube_func.py
+++ tests/unit/test_youtube_func.py
@@ -1,7 +1,6 @@
 import pytest
-from src.IPYNBrenderer import get_time_info
+from src.IPYNBrenderer.youtube import get_time_info
 from src.IPYNBrenderer.custom_exception import InvalidURLException
-
 
 
 good_URL_data = [
@@ -15,16 +14,21 @@ URL_id_bad_data = [
     ("https://www.youtube.com/watch?v=roO5VGxOw2sahesbf"),  # exception
     ("https://www.youtube.com/watch?v=roO5VGxOw2s&t"),  # exception
     ("https://www.youtube.com/watch?v=roO5VGxOw2s&t==22s"),  # exception
-    ("https://www.youtube.com/watch?v==roO5VGxOw2s&t=22s")
+    ("https://www.youtube.com/watch?v==roO5VGxOw2s&t=22s"),
 ]
 
 
-@pytest.mark.parametrize("URL, response", good_URL_data)# This decorator tells pytest to run the test function multiple times, each time with different parameters specified in the good_URL_data list
+@pytest.mark.parametrize(
+    "URL, response", good_URL_data
+)  # This decorator tells pytest to run the test function multiple times, each time with different parameters specified in the good_URL_data list
 def test_get_time_info(URL, response):
     assert get_time_info(URL) == response
 
+
 """ This test function verifies that the get_time_info function correctly raises an InvalidURLException 
     when provided with invalid YouTube URLs. It achieves this by using the pytest.raises context manager to check for the expected exception during the execution of the test."""
+
+
 @pytest.mark.parametrize("URL", URL_id_bad_data)
 def test_get_time_info_failed(URL):
     with pytest.raises(InvalidURLException):
